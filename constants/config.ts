5afc5055e4fa939b4058ebab897526c0ede8f57b
ramizaiter1989 <ramizaiter1989@gmail.com>
2025-10-10 22:40:53 +0200

    after trying to fix push problem- test new api

--- constants/config.ts
+++ constants/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://dfe1a98ffdd4.ngrok-free.app/api";
+export const API_BASE_URL = "https://b4d50a19dba0.ngrok-free.app/api";
 export const ORS_API_KEY="eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjIxODBmOTIzMjI1NzQ0YTFiMjc4NmYxMmJkZmQzNzlkIiwiaCI6Im11cm11cjY0In0=:";
 export const USE_OSRM = true;
